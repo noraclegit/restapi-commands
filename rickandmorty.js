ebd996ab078b88c13687aaaabbd39fd499315bb4
Lesi (lastname) <72552172+lesigit@users.noreply.github.com>
2021-03-14 14:29:26 +0530

    Create rickandmorty.js
    Command to search up any rick and morty character

---
+++ rickandmorty.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch')
+const {MessageEmbed} = require('discord.js')
+module.exports={
+    name: 'rickandmorty',
+    aliases: ["rickmorty","rick&morty","ricknmorty","rick","morty","rnm"],
+    description: "Search any Rick And Morty character",
+    async run(bot,message,args){
+        const res = await fetch(`https://rickandmortyapi.com/api/character?name=${args.join("%20")}`).then(rnm => rnm.json())
+        if(res.error == "There is nothing here") return message.channel.send(res.error)
+        const firstres = res.results[0]
+        const epcount = []
+        firstres.episode.forEach(ep =>{
+            const e = ep.slice("https://rickandmortyapi.com/api/episode/".length)
+            
+            epcount.push(`Episode ${e}`)
+        })
+        message.channel.send(new MessageEmbed()
+                            .setTitle(firstres.name)
+                             .setDescription(`Status: ${firstres.status} \n Species: ${firstres.species} \n Gender: ${firstres.gender} \n Episodes: ${epcount.join(",")} \n Universe: ${firstres.location.name}`)
+                             .setThumbnail(firstres.image)
+                             .setFooter("https://rickandmortyapi.com")
+        )
+    }
+}
